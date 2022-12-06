dcbb6846fd0030cefdb751ad162b094ec937c72b
pandy <pandy@192.168.0.20>
2022-12-05 16:23:21 -0800

    First

---
+++ ChallangeOne/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	public static void main(String[] args) {
+   		int count = 0;
+//		while(count != 5) {
+//			System.out.println("Count value is " + count);
+//			count++;
+//		}
+		
+		do {
+			System.out.println("Count value was " + count);
+			count++;
+		}while(count != 6);
+	}
+}
